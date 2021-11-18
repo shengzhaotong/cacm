c3ec7adbfc742c31644043be3a085806be377007
shengzhaotong <1454579997@qq.com>
2021-11-18 20:12:34 +0800

    commit

--- src/test/java/com/lab/BackApplicationTests.java
+++ src/test/java/com/lab/BackApplicationTests.java
@@ -1,36 +1,19 @@
 package com.lab;
 
-import com.lab.utils.Cache;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 
 @SpringBootTest
 class BackApplicationTests {
 
-
-    @Autowired
-    Cache cache;
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Test
-    void contextLoads() throws Exception{
-        cache.set("key","123");
-        System.out.println(cache.get("key"));
+    void contextLoads() {
     }
 
-    @GetMapping("/test")
-    public String test (HttpServletRequest request) {
-        String header = request.getHeader("User-Agent");
-        System.out.println(header);
-        return "test";
-    }
 
 }
