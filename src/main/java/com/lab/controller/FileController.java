dadd91b89fb975b07b7788d3c25fb21dfb095062
shengzhaotong <1454579997@qq.com>
2022-05-21 16:02:32 +0800

    c

--- src/main/java/com/lab/controller/FileController.java
+++ src/main/java/com/lab/controller/FileController.java
@@ -2,10 +2,7 @@ package com.lab.controller;
 
 import com.lab.utils.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FileController {
@@ -13,23 +10,23 @@ public class FileController {
     @Autowired
     FileUtil fileUtil;
 
-    @PostMapping("/file_upload")
-    public String fileUpload (@RequestParam("file") MultipartFile file) throws Exception{
-        String fileName = file.getOriginalFilename();
-        if("".equals(fileName) || fileName == null){
-            return "the file is null";
-        }
-        if(fileUtil.judgeFileSize(file)>5.0){
-            return "请上传5M以内的文件";
-        }
-
-        StringBuilder buffer = new StringBuilder();
-
-        buffer.append("/home/resume/");
-        buffer.append(fileName);
-        String path = new String(buffer);
-
-        return fileUtil.fileUpload(file, path);
-    }
+//    @PostMapping("/file_upload")
+//    public String fileUpload (@RequestParam("file") MultipartFile file) throws Exception{
+//        String fileName = file.getOriginalFilename();
+//        if("".equals(fileName) || fileName == null){
+//            return "the file is null";
+//        }
+//        if(fileUtil.judgeFileSize(file)>5.0){
+//            return "请上传5M以内的文件";
+//        }
+//
+//        StringBuilder buffer = new StringBuilder();
+//
+//        buffer.append("/home/resume/");
+//        buffer.append(fileName);
+//        String path = new String(buffer);
+//
+//        return fileUtil.fileUpload(file, path);
+//    }
 
 }
